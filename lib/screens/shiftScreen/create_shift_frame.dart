db411c7f44cc32618203a9b10b3b253dd2b39482
kaku-panda <program.kaku.panda@gmail.com>
2024-05-30 23:59:54 +0900

    test

--- lib/screens/shiftScreen/create_shift_frame.dart
+++ lib/screens/shiftScreen/create_shift_frame.dart
@@ -204,6 +204,8 @@ class CreateShiftFramePageState extends ConsumerState<CreateShiftFramePage>
       ),
     );
   }
+a
+
 
   ////////////////////////////////////////////////////////////////////////////////////////////
   ///  シフト管理画面の使い方を説明するための関数
